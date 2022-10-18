9b2f92b31e58bad2e11c47f244d1f95583fa1177
izar06 <izar.anam22@gmail.com>
2022-10-18 15:50:51 +0700

    update config and format API

--- src/main/java/com/thrive/GreetingResource.java
+++ src/main/java/com/thrive/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.thrive;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,10 +9,12 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-
+    @ConfigProperty(name ="greeting.message")
+    String myName;
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from "+myName;
     }
 }
